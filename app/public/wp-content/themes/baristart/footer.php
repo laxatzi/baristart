3b9185738aa406339acb172b2e0ead50e00c0eb3
laxatzi <laxatzi@yahoo.com>
2026-05-21 13:18:36 +0300

    add cols and rows

--- app/public/wp-content/themes/baristart/footer.php
+++ app/public/wp-content/themes/baristart/footer.php
@@ -13,17 +13,27 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'baristart' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'baristart' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Developed by: %1$s.', 'baristart' ), '<a href="https://lambroshatzinikolaou.com">Lambros Hatzinikolaou</a>' );
-				?>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-4 col-12 me-auto">
+						<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'baristart' ) ); ?>">
+							<?php
+							/* translators: %s: CMS name, i.e. WordPress. */
+							printf( esc_html__( 'Proudly powered by %s', 'baristart' ), 'WordPress' );
+							?>
+						</a>
+						<span class="sep"> | </span>
+							<?php
+							/* translators: 1: Theme name, 2: Theme author. */
+							printf( esc_html__( 'Developed by: %1$s.', 'baristart' ), '<a href="https://lambroshatzinikolaou.com">Lambros Hatzinikolaou</a>' );
+							?>
+
+					</div>
+
+				</div><!-- .row -->
+
+			</div><!-- .container -->
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
