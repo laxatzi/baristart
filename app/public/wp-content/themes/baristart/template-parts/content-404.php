c4893dc781db24b18b9b097b99e474b44ce1b853
laxatzi <laxatzi@yahoo.com>
2026-05-22 15:46:58 +0300

    Add padding in page content

--- app/public/wp-content/themes/baristart/template-parts/content-404.php
+++ app/public/wp-content/themes/baristart/template-parts/content-404.php
@@ -1,13 +1,13 @@
 <header class="page-header">
   <h1 class="page-title" id="error-404-title">
-    <?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'baristart' ); ?>
+    <?php echo sprintf( esc_html__( '%s! That page can&rsquo;t be found.', 'baristart' ), '<span class="fw-semibold">' . esc_html__( 'Oops', 'baristart' ) . '</span>' ); ?>
   </h1>
 </header><!-- .page-header -->
-<div class="page-content">
+<div class="page-content pb-5">
       <!-- Explanation text -->
     <div class="error-explanation">
       <p>
-        <?php esc_html_e( 'The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'LambrosPersonalTheme' ); ?>
+        <?php esc_html_e( 'The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'baristart' ); ?>
       </p>
     </div>
       <!-- Search form -->
