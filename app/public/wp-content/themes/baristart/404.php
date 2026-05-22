b5f17f1c76a64a121daf4fa7497fbe2c1aac112f
laxatzi <laxatzi@yahoo.com>
2026-05-22 13:13:46 +0300

    add aria-labelledby to title

--- app/public/wp-content/themes/baristart/404.php
+++ app/public/wp-content/themes/baristart/404.php
@@ -21,11 +21,11 @@ get_header();
 
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main error-404-main" aria-labelledby="error-404-title">
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'baristart' ); ?></h1>
+				<h1 class="page-title" id="error-404-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'baristart' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
