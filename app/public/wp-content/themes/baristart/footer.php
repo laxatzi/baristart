8d26467bfa4768d2f384303ccb96bec9278cb009
laxatzi <laxatzi@yahoo.com>
2026-05-25 11:33:25 +0300

    change hover styles in contact details on footer.php

--- app/public/wp-content/themes/baristart/footer.php
+++ app/public/wp-content/themes/baristart/footer.php
@@ -44,14 +44,14 @@
 					<div class="col-lg-3 col-12 mt-4 mb-3 mt-lg-0 mb-lg-0">
             <em class="text-white d-block mb-4">Contact</em>
 						<p class="d-flex mb-1">
-              <strong class="me-2">Phone:</strong>
-              <a href="tel: 305-240-9671" class="site-footer-link">
+              <strong class="me-2 text-white">Phone:</strong>
+              <a href="tel: 305-240-9671" class="site-footer-link" data-text="(65) 305 2409 671">
                 (65) 305 2409 671
               </a>
             </p>
 						<p class="d-flex">
-              <strong class="me-2">Email:</strong>
-							<a href="mailto:info@yourgmail.com" class="site-footer-link">
+              <strong class="me-2 text-white">Email:</strong>
+							<a href="mailto:info@yourgmail.com" class="site-footer-link" data-text="hello@barista.co">
                 hello@barista.co
               </a>
             </p>
