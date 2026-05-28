4d813b817479dbaedb32dc1a5e70e3806b52f8b0
laxatzi <laxatzi@yahoo.com>
2026-05-28 10:01:29 +0300

    add margins to components

--- app/public/wp-content/themes/baristart/template-parts/hero-section.php
+++ app/public/wp-content/themes/baristart/template-parts/hero-section.php
@@ -1,6 +1,6 @@
 <?php
  ?>
-<section id="section_1" class="hero-section">
+<section id="section_1" class="hero-section d-flex align-items-center justify-content-center">
   <div class="container">
     <div class="row align-items-center">
       <div class="col-lg-6 col-12 mx-auto">
@@ -9,8 +9,8 @@
         </em>
         <h1>Baristart Cafe</h1>
         <p class="text-white mb-4 pb-lg-2">Best coffee in town</p>
-        <a href="" class="btn custom-btn">Our Story</a>
-        <a href="" class="btn custom-btn">Check Menu</a>
+        <a href="" class="btn custom-btn smoothscroll me-3">Our Story</a>
+        <a href="" class="btn custom-btn smoothscroll me-2 mb-2"><strong>Check Menu</strong></a>
       </div>
     </div>
   </div>
