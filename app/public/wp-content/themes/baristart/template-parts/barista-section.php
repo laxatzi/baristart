d32166317a2dfd829de3c98d0e29cb9f0c93d44d
laxatzi <laxatzi@yahoo.com>
2026-06-03 13:41:49 +0300

    add barista section template part

---
+++ app/public/wp-content/themes/baristart/template-parts/barista-section.php
@@ -0,0 +1,11 @@
+<section class="barista-section">
+  <div class="section-overlay"></div>
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-12 text-center">
+        <em class="small-text text-white">Our Coffee Experts</em>
+        <h2 class="text-white">Meet Our Baristas</h2>
+      </div>
+    </div>
+  </div>
+</section>
