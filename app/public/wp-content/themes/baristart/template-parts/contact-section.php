11ff66d2bb5d42d7b602587db0a1483da28d7def
laxatzi <laxatzi@yahoo.com>
2026-06-04 22:05:25 +0300

    create form

--- app/public/wp-content/themes/baristart/template-parts/contact-section.php
+++ app/public/wp-content/themes/baristart/template-parts/contact-section.php
@@ -7,6 +7,28 @@
           Contact Us
         </h2>
       </div>
+      <div class="col-lg-6 col-12">
+        <form action="#" method="post" class="custom-form contact-form" role="form">
+          <div class="row">
+            <div class="col-lg-6 col-12">
+              <label for="name" class="form-label">Name <sup class="text-danger">*</sup></label>
+              <input type="text" name="name" id="name" class="form-control" placeholder="Jackson" required="">
+            </div>
+            <div class="col-lg-6 col-12">
+              <label for="email" class="form-label">Email Address</label>
+              <input type="email" name="email" id="email" pattern="[^ @]*@[^ @]*" class="form-control" placeholder="Jack@gmail.com" required="">
+            </div>
+            <div class="col-12">
+              <label for="message" class="form-label">How can we help?</label>
+              <textarea name="message" rows="4" class="form-control" id="message" placeholder="Message" required=""></textarea>
+            </div>
+          </div>
+
+          <div class="col-lg-5 col-12 mx-auto mt-3">
+            <button type="submit" class="form-control">Send Message</button>
+          </div>
+       </form>
+      </div>
     </div>
   </div>
 </section>
