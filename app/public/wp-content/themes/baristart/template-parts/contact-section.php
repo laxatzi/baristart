1ee236b956407157835d1405e3792b6a3db8057b
laxatzi <laxatzi@yahoo.com>
2026-06-04 22:36:55 +0300

    google map in iframe

--- app/public/wp-content/themes/baristart/template-parts/contact-section.php
+++ app/public/wp-content/themes/baristart/template-parts/contact-section.php
@@ -29,6 +29,9 @@
           </div>
        </form>
       </div>
+      <div class="col-lg-6 col-12">
+        <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5039.668141741662!2d72.81814769288509!3d19.043340656729775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c994f34a7355%3A0x2680d63a6f7e33c2!2sLover%20Point!5e1!3m2!1sen!2sth!4v1692722771770!5m2!1sen!2sth" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
     </div>
   </div>
 </section>
