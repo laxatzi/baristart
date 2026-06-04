a0cd040251fb8c9aa6b8a7c1ed3bd70cc0e233f7
laxatzi <laxatzi@yahoo.com>
2026-06-04 17:17:30 +0300

    add margin bottom to testimonials heading

--- app/public/wp-content/themes/baristart/template-parts/testimonials-section.php
+++ app/public/wp-content/themes/baristart/template-parts/testimonials-section.php
@@ -1,7 +1,7 @@
 <section class="testimonials-section section-padding" id="section_testimonials">
   <div class="container">
     <div class="row justify-content-center">
-      <div class="col-lg-12 col-12 text-center mb-4 pb-lg-2">
+      <div class="col-lg-12 col-12 text-center mb-5 pb-lg-2">
         <em class="small-text text-white">What Our Customers Say</em>
         <h2 class="text-white">Testimonials</h2>
       </div>
