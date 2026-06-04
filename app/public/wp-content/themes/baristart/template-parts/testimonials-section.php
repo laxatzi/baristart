ff2d446ee804c35184eff6055cfcbbe3d8488273
laxatzi <laxatzi@yahoo.com>
2026-06-04 14:06:24 +0300

    buld testimonials section

--- app/public/wp-content/themes/baristart/template-parts/testimonials-section.php
+++ app/public/wp-content/themes/baristart/template-parts/testimonials-section.php
@@ -2,9 +2,41 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-lg-12 col-12 text-center mb-4 pb-lg-2">
-        <em class="small-text text-white">What Our Customers Say</em>
-        <h2 class="text-white">Testimonials</h2>
+        <em class="small-text">What Our Customers Say</em>
+        <h2>Testimonials</h2>
       </div>
     </div>
+    <div class="timeline">
+      <div class="timeline-container timeline-container-left">
+        <div class="timeline-content">
+        <div class="reviews">
+          <div class="reviews-img-wrapper">
+            <img src="" alt="">
+          </div>
+        <div class="reviews-info">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <div class="d-flex border-top pt-3 mt-4">
+            <strong class="text-white">"4.5"
+              <small class="ms-2">Rating</small>
+            </strong>
+           <div class="reviews-group">
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star"></i>
+           </div>
+         </div>
+    <!-- reviews-info -->
+       </div>
+   <!-- reviews -->
+      </div>
+   <!-- timeline-content -->
+      </div>
+    <!-- timeline-container -->
+   </div>
+   <!-- timeline -->
+  </div>
+  <!-- container -->
   </div>
 </section>
