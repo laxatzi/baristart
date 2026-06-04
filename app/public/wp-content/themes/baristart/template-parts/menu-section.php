9186a47f7c3c5c826de7393c906bae09862438f7
laxatzi <laxatzi@yahoo.com>
2026-06-04 10:24:43 +0300

    track menu-section template

---
+++ app/public/wp-content/themes/baristart/template-parts/menu-section.php
@@ -0,0 +1,47 @@
+<section class="menu-section section-padding" id="section_menu">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-6 col-12 text-center mb-4 mb-lg-0">
+             <div class="menu-wrapper ">
+          <div class="food-menu-header text-center mb-4 pb-lg-2">
+            <em class="text-white ">First Grade Breakfast</em>
+            <h4 class="text-white ">Breakfast Specials</h4>
+          </div>
+          <div class="food-menu">
+            <div class="d-flex">
+              <h6>Toast Sandwich</h6>
+              <span class="underline"></span>
+              <strong class="ms-auto">$5.99</strong>
+            </div>
+                      <div class="border-top mt-2 pt-2">
+            <small>Top quality ingredients</small>
+          </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="col-lg-6 col-12">
+        <div class="menu-wrapper ">
+          <div class="food-menu-header text-center mb-4 pb-lg-2">
+            <em class="text-white ">First Grade Coffee</em>
+            <h4 class="text-white ">Coffee Specials</h4>
+          </div>
+          <div class="food-menu">
+            <div class="d-flex">
+              <h6>Latte</h6>
+              <span class="underline"></span>
+              <strong class="ms-auto">$3.99</strong>
+            </div>
+                      <div class="border-top mt-2 pt-2">
+            <small>Fresh brewed coffee and steamed milk</small>
+          </div>
+          </div>
+
+        </div>
+        <!-- end of col -->
+      </div>
+
+    </div>
+  </div>
+</section>
