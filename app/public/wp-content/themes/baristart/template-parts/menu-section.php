bcc38db9e4c55596a7023a3cb61659d5fc4f9f33
laxatzi <laxatzi@yahoo.com>
2026-06-04 11:36:05 +0300

    add food items in menu with details

--- app/public/wp-content/themes/baristart/template-parts/menu-section.php
+++ app/public/wp-content/themes/baristart/template-parts/menu-section.php
@@ -9,7 +9,7 @@
           </div>
           <div class="food-menu">
             <div class="d-flex">
-              <h6>Toast Sandwich</h6>
+              <h6 class="text-white">Toast Sandwich</h6>
               <span class="underline"></span>
               <strong class="ms-auto">$5.99</strong>
             </div>
@@ -17,6 +17,51 @@
               <small>Top quality ingredients</small>
             </div>
           </div>
+       <!-- end of menu item -->
+                  <div class="food-menu">
+            <div class="d-flex">
+              <h6 class="text-white">Toast Sandwich</h6>
+              <span class="underline"></span>
+              <strong class="ms-auto">$5.99</strong>
+            </div>
+            <div class="border-top mt-2 pt-2 text-start">
+              <small>Top quality ingredients</small>
+            </div>
+          </div>
+       <!-- end of menu item -->
+          <div class="food-menu">
+            <div class="d-flex">
+              <h6 class="text-white">Waffle</h6>
+              <span class="underline"></span>
+              <strong class="ms-auto">$4.99</strong>
+            </div>
+            <div class="border-top mt-2 pt-2 text-start">
+              <small>The classic waffle with syrup</small>
+            </div>
+          </div>
+       <!-- end of menu item -->
+          <div class="food-menu">
+            <div class="d-flex">
+              <h6 class="text-white">Spanakopita</h6>
+              <span class="underline"></span>
+              <strong class="ms-auto">$5.99</strong>
+            </div>
+            <div class="border-top mt-2 pt-2 text-start">
+              <small>Traditional Greek spinach pie</small>
+            </div>
+          </div>
+       <!-- end of menu item -->
+          <div class="food-menu">
+            <div class="d-flex">
+              <h6 class="text-white">Pancakes</h6>
+              <span class="underline"></span>
+              <strong class="ms-auto">$4.99</strong>
+            </div>
+            <div class="border-top mt-2 pt-2 text-start">
+              <small>Fluffy pancakes with syrup</small>
+            </div>
+          </div>
+       <!-- end of menu item -->
 
         </div>
       </div>
@@ -29,7 +74,7 @@
           </div>
           <div class="food-menu">
             <div class="d-flex">
-              <h6>Latte</h6>
+              <h6 class="text-white">Latte</h6>
               <span class="underline"></span>
               <strong class="ms-auto">$3.99</strong>
             </div>
@@ -37,6 +82,55 @@
             <small>Fresh brewed coffee and steamed milk</small>
           </div>
           </div>
+          <!-- end of menu item -->
+
+          <div class="food-menu">
+            <div class="d-flex">
+              <h6 class="text-white">Cappuccino</h6>
+              <span class="underline"></span>
+              <strong class="ms-auto">$4.49</strong>
+            </div>
+            <div class="border-top mt-2 pt-2">
+              <small>Italian espresso with steamed milk</small>
+            </div>
+          </div>
+          <!-- end of menu item -->
+
+           <div class="food-menu">
+            <div class="d-flex">
+              <h6 class="text-white">Americano</h6>
+              <span class="underline"></span>
+              <strong class="ms-auto">$2.99</strong>
+            </div>
+            <div class="border-top mt-2 pt-2">
+              <small>The classic espresso with hot water</small>
+            </div>
+          </div>
+          <!-- end of menu item -->
+
+          <div class="food-menu">
+            <div class="d-flex">
+              <h6 class="text-white">Dark Chocolate</h6>
+              <span class="underline"></span>
+              <strong class="ms-auto">$4.99</strong>
+            </div>
+            <div class="border-top mt-2 pt-2">
+              <small>Fresh brewed dark chocolate</small>
+            </div>
+          </div>
+          <!-- end of menu item -->
+
+          <div class="food-menu">
+            <div class="d-flex">
+              <h6 class="text-white">Green Tea</h6>
+              <span class="underline"></span>
+              <strong class="ms-auto">$3.99</strong>
+            </div>
+            <div class="border-top mt-2 pt-2">
+              <small>Fresh green tea</small>
+            </div>
+          </div>
+          <!-- end of menu item -->
 
         </div>
         <!-- end of col -->
