c659f06fb34d1393f4394d6a4e17e9baa5652ae3
laxatzi <laxatzi@yahoo.com>
2026-06-04 14:46:26 +0300

    add two more testimonials

--- app/public/wp-content/themes/baristart/template-parts/testimonials-section.php
+++ app/public/wp-content/themes/baristart/template-parts/testimonials-section.php
@@ -35,6 +35,64 @@
       </div>
     <!-- timeline-container -->
    </div>
+
+         <div class="timeline-container timeline-container-right">
+        <div class="timeline-content">
+        <div class="reviews">
+          <div class="reviews-img-wrapper">
+            <img src="" alt="">
+          </div>
+        <div class="reviews-info">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <div class="d-flex border-top pt-3 mt-4">
+            <strong class="text-white">"4.5"
+              <small class="ms-2">Rating</small>
+            </strong>
+           <div class="reviews-group">
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star"></i>
+           </div>
+         </div>
+    <!-- reviews-info -->
+       </div>
+   <!-- reviews -->
+      </div>
+   <!-- timeline-content -->
+      </div>
+    <!-- timeline-container -->
+   </div>
+
+         <div class="timeline-container timeline-container-left">
+        <div class="timeline-content">
+        <div class="reviews">
+          <div class="reviews-img-wrapper">
+            <img src="" alt="">
+          </div>
+        <div class="reviews-info">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <div class="d-flex border-top pt-3 mt-4">
+            <strong class="text-white">"4.5"
+              <small class="ms-2">Rating</small>
+            </strong>
+           <div class="reviews-group">
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star"></i>
+           </div>
+         </div>
+    <!-- reviews-info -->
+       </div>
+   <!-- reviews -->
+      </div>
+   <!-- timeline-content -->
+      </div>
+    <!-- timeline-container -->
+   </div>
    <!-- timeline -->
   </div>
   <!-- container -->
