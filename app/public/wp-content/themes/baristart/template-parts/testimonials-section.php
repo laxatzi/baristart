ed4370d2e38b606989d26ad267a1b3a5ff6a031f
laxatzi <laxatzi@yahoo.com>
2026-06-04 14:15:40 +0300

    style timeline

--- app/public/wp-content/themes/baristart/template-parts/testimonials-section.php
+++ app/public/wp-content/themes/baristart/template-parts/testimonials-section.php
@@ -2,8 +2,8 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-lg-12 col-12 text-center mb-4 pb-lg-2">
-        <em class="small-text">What Our Customers Say</em>
-        <h2>Testimonials</h2>
+        <em class="small-text text-white">What Our Customers Say</em>
+        <h2 class="text-white">Testimonials</h2>
       </div>
     </div>
     <div class="timeline">
