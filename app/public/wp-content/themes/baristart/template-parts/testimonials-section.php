a450b78fc5a242915f1bd50b0155ad982eaec460
laxatzi <laxatzi@yahoo.com>
2026-06-04 11:48:40 +0300

    add testimonials template

---
+++ app/public/wp-content/themes/baristart/template-parts/testimonials-section.php
@@ -0,0 +1,10 @@
+<section class="testimonials-section section-padding" id="section_testimonials">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-12 col-12 text-center mb-4 pb-lg-2">
+        <em class="small-text text-white">What Our Customers Say</em>
+        <h2 class="text-white">Testimonials</h2>
+      </div>
+    </div>
+  </div>
+</section>
