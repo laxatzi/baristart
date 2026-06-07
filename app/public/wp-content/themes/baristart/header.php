2a0deff9a01fece431de550218453503b548c191
laxatzi <laxatzi@yahoo.com>
2026-06-07 19:03:15 +0300

    add navbar menu items

--- app/public/wp-content/themes/baristart/header.php
+++ app/public/wp-content/themes/baristart/header.php
@@ -48,6 +48,26 @@
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
        </button>
+			 <div class="collapse navbar-collapse" id="navbarNav">
+         <ul class="navbar-nav ms-lg-auto">
+           <li class="nav-item">
+             <a class="nav-link click-scroll active" href="#section_1">Home</a>
+           </li>
+					 <li class="nav-item">
+              <a class="nav-link click-scroll inactive" href="#section_2">About</a>
+           </li>
+					 <li class="nav-item">
+            <a class="nav-link click-scroll inactive" href="#section_3">Our Menu</a>
+           </li>
+					 <li class="nav-item">
+             <a class="nav-link click-scroll inactive" href="#section_4">Reviews</a>
+          </li>
+					<li class="nav-item">
+            <a class="nav-link click-scroll inactive" href="#section_5">Contact</a>
+          </li>
+        </ul>
+
+			</div>
 		</div>
 	</nav>
 </div>
