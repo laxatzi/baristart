fcfd8ca54bf0411a73f4653a510ab10fd3df5d85
laxatzi <laxatzi@yahoo.com>
2026-06-08 12:41:38 +0300

    add hero-info background shadow

--- app/public/wp-content/themes/baristart/template-parts/hero-section.php
+++ app/public/wp-content/themes/baristart/template-parts/hero-section.php
@@ -3,14 +3,14 @@
 
   <div class="container">
     <div class="row align-items-center">
-      <div class="col-lg-6 col-12 mx-auto">
+      <div class="col-lg-6 col-12 mx-auto hero-info">
         <em class="small-text">
           Welcome to our Cafe
         </em>
         <h1>Baristart Cafe</h1>
         <p class="text-white mb-4 pb-lg-2">Best coffee in town</p>
         <a href="" class="btn custom-btn smoothscroll me-3">Our Story</a>
-        <a href="" class="btn custom-btn smoothscroll me-2 mb-2"><strong>Check Menu</strong></a>
+        <a href="" class="btn custom-btn custom-btn--distinct smoothscroll me-2 mb-2"><strong>Check Menu</strong></a>
       </div>
     </div>
   </div>
