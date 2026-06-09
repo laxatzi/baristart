32611aeee12c84cd885f1bd46a0bf1625a6b0061
laxatzi <laxatzi@yahoo.com>
2026-06-09 17:00:59 +0300

    create page-reservation page

--- app/public/wp-content/themes/baristart/page-reservation.php
+++ app/public/wp-content/themes/baristart/page-reservation.php
@@ -3,19 +3,15 @@
 get_header();
 ?>
   <main id="primary" class="site-main reservation-main text-white" aria-labelledby="reservation-title">
+    <?php
+    get_template_part( 'template-parts/hero-section' );
+
+    ?>
 
     <div class="container">
-    <section class="reservation-section row pb-5 pt-5">
-      <h2>This is the reservation page</h2>
-        <?php
-    /**
-     * A custom action hook that fires before the reservation content. Enables coder (or a plugin) to inject content before the main message without editing this file directly — for example, a banner or breadcrumb.
-     *
-     * @since 1.0.0
-     */
-    do_action( 'baristart_before_reservation_content' );
-    ?>
-    </section><!-- .reservation-section -->
+     <?php
+       get_template_part( 'template-parts/reservation-form' );
+     ?>
    </div>
 
   </main>
