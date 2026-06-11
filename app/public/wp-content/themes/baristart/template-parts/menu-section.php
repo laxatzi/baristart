b601b091159bef572d507016bb762eb8ac476f04
laxatzi <laxatzi@yahoo.com>
2026-06-11 13:20:37 +0300

    add link leading to main page

--- app/public/wp-content/themes/baristart/template-parts/menu-section.php
+++ app/public/wp-content/themes/baristart/template-parts/menu-section.php
@@ -137,5 +137,6 @@
       </div>
 
     </div>
+      <input class="mx-auto d-block p-2 mt-5 col-lg-4 col-12"type="button" value="View Full Menu" onclick="window.location.href='#'">
   </div>
 </section>
