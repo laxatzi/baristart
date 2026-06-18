2a58bfe60fb540957e48211a1453d669e01b0384
laxatzi <laxatzi@yahoo.com>
2026-06-18 15:46:36 +0300

    add reviews

--- app/public/wp-content/themes/baristart/page-reviews.php
+++ app/public/wp-content/themes/baristart/page-reviews.php
@@ -1,9 +1,120 @@
 <?php
 get_header();
 ?>
-<?php
- get_template_part( 'template-parts/testimonials-section' );
-?>
+<section class="testimonials-section section-padding" id="section_testimonials">
+  <div class="container">
+    <div class="row justify-content-center pt-5 mt-5">
+      <div class="col-lg-12 col-12 text-center mb-5 pb-lg-2">
+        <em class="small-text text-white">What Our Customers Say</em>
+        <h2 class="text-white">Testimonials</h2>
+      </div>
+    </div>
+    <div class="timeline">
+      <div class="timeline-container timeline-container-left">
+        <div class="timeline-content">
+        <div class="reviews">
+          <div class="reviews-img-wrapper d-flex align-items-center">
+            <img src="<?php echo esc_url( get_template_directory_uri() . '/assets/woman-with-yellow-sweater.jpg' ); ?>" alt="Sandra" class="img-fluid reviews-image">
+            <div class="img-name">
+              <h6 class="text-white">Sandra</h6>
+              <em class="text-white">Customer</em>
+            </div>
+          </div>
+        <div class="reviews-info">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <div class="d-flex border-top pt-3 mt-4">
+            <strong class="text-white">"4.5"
+              <small class="ms-2">Rating</small>
+            </strong>
+           <div class="reviews-group">
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star"></i>
+           </div>
+         </div>
+    <!-- reviews-info -->
+       </div>
+   <!-- reviews -->
+      </div>
+   <!-- timeline-content -->
+      </div>
+    <!-- timeline-container -->
+   </div>
+
+         <div class="timeline-container timeline-container-right">
+        <div class="timeline-content">
+        <div class="reviews">
+          <div class="reviews-img-wrapper d-flex align-items-center">
+            <img src="<?php echo esc_url( get_template_directory_uri() . '/assets/man-white-sweater.jpg' ); ?>" alt="John" class="img-fluid reviews-image">
+            <div class="img-name">
+              <h6 class="text-white">John</h6>
+              <em class="text-white">Customer</em>
+            </div>
+          </div>
+        <div class="reviews-info">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <div class="d-flex border-top pt-3 mt-4 justify-content-between">
+            <strong class="text-white">"4.5"
+              <small class="ms-2">Rating</small>
+            </strong>
+           <div class="reviews-group">
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star"></i>
+           </div>
+         </div>
+    <!-- reviews-info -->
+       </div>
+   <!-- reviews -->
+      </div>
+   <!-- timeline-content -->
+      </div>
+    <!-- timeline-container -->
+   </div>
+
+         <div class="timeline-container timeline-container-left">
+        <div class="timeline-content">
+        <div class="reviews">
+          <div class="reviews-img-wrapper d-flex align-items-center">
+            <img src="<?php echo esc_url( get_template_directory_uri() . '/assets/woman-pink-warm-sweater.jpg' ); ?>" alt="Emily" class="img-fluid reviews-image">
+            <div class="img-name">
+              <h6 class="text-white">Emily</h6>
+              <em class="text-white">Customer</em>
+            </div>
+          </div>
+        <div class="reviews-info">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <div class="d-flex border-top pt-3 mt-4">
+            <strong class="text-white">"4.5"
+              <small class="ms-2">Rating</small>
+            </strong>
+           <div class="reviews-group">
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star-fill"></i>
+             <i class="bi-star"></i>
+           </div>
+         </div>
+    <!-- reviews-info -->
+       </div>
+   <!-- reviews -->
+      </div>
+   <!-- timeline-content -->
+      </div>
+    <!-- timeline-container -->
+   </div>
+   <!-- timeline -->
+  </div>
+
+
+  <!-- container -->
+  </div>
+</section>
 <?php
 get_footer();
 ?>
