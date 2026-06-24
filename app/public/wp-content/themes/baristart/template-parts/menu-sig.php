5284ed635dfdaa429a2cddea49f3246936b3621d
laxatzi <laxatzi@yahoo.com>
2026-06-24 13:51:39 +0300

    refactor sig

--- app/public/wp-content/themes/baristart/template-parts/menu-sig.php
+++ app/public/wp-content/themes/baristart/template-parts/menu-sig.php
@@ -1,7 +1,19 @@
-						<div class="col-lg-8 col-12 mt-4 copyright-text">
-							<?php
-							/* translators: 1: Theme name, 2: Theme author. */
-							printf( esc_html__( 'Developed by: %1$s.', 'baristart' ), '<a href="https://lambroshatzinikolaou.com">Lambros Hatzinikolaou</a>' );
-							?>
-              <p class="copyright-text mb-0">Copyright © Baristart 2026. All rights reserved.</p>
-            </div>
+<div class="col-lg-8 col-12 mt-4 copyright-text">
+	<?php
+    /**
+       * Displays the footer copyright information with dynamic year.
+       *
+       * Outputs a translatable copyright notice that includes the theme author name
+       * and the current year. The content is properly escaped for security.
+       *
+       * @since 1.0.0
+       *
+       * @return void Echoes the copyright text directly to output.
+       */
+      printf(
+        esc_html__( 'Developed by %1$s © 2024 - %2$s. All rights reserved.', 'LambrosPersonalTheme' ),
+        '<span class="sig__span">' . esc_html( LAMBROS_THEME_AUTHOR ) . '</span>',
+        esc_html( wp_date( 'Y' ) )
+      );
+  ?>
+</div>
