0bdac4e1dfd8edb163391a1a43b59916a6fbf1f7
laxatzi <laxatzi@yahoo.com>
2026-06-24 10:47:19 +0300

    create sig template

---
+++ app/public/wp-content/themes/baristart/template-parts/menu-sig.php
@@ -0,0 +1,7 @@
+						<div class="col-lg-8 col-12 mt-4 copyright-text">
+							<?php
+							/* translators: 1: Theme name, 2: Theme author. */
+							printf( esc_html__( 'Developed by: %1$s.', 'baristart' ), '<a href="https://lambroshatzinikolaou.com">Lambros Hatzinikolaou</a>' );
+							?>
+              <p class="copyright-text mb-0">Copyright © Baristart 2026. All rights reserved.</p>
+            </div>
